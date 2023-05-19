dd90d6a4911c3b3a0790c2b62701e235e047bd34
rahul-pt6281 <rahulraj94391@gmail.com>
2023-05-19 11:33:51 +0530

    merged to main and deleted the 'drawer' branch

--- app/src/main/java/com/example/mall/MSharedPreferences.kt
+++ app/src/main/java/com/example/mall/MSharedPreferences.kt
@@ -6,6 +6,5 @@ object MSharedPreferences {
     const val IS_LOGGED_IN = "is_logged_in"     // true or false
     const val LOGGED_IN_USER_ID = "uid"     // int
     const val IS_FIRST_TIME = "is_first_time"   // true/False
-
 }
 
